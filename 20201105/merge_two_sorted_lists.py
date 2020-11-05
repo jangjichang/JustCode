f737bf61e5fef417a66c9fdc5df2800502846689
Jichang Jang <devjang@crossangle.io>
2020-11-05 20:56:20 +0900

    Add problem

---
+++ 20201105/merge_two_sorted_lists.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        return 0
+
+def test_solution():
+    solution = Solution()
+    assert solution.mergeTwoLists(ListNode(), ListNode()) == 1
