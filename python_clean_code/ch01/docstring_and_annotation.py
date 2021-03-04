ebe1c198488446a2ea727ec9acf15ba5cb1a320f
JichangJang <wlckd90@gmail.com>
2021-03-04 22:10:17 +0900

    표준 라이브러리에 docstring을 사용하는 좋은 예 확인

---
+++ python_clean_code/ch01/docstring_and_annotation.py
@@ -0,0 +1,8 @@
+print(dict.update.__doc__)
+
+"""
+D.update([E, ]**F) -> None.  Update D from dict/iterable E and F.
+If E is present and has a .keys() method, then does:  for k in E: D[k] = E[k]
+If E is present and lacks a .keys() method, then does:  for k, v in E: D[k] = v
+In either case, this is followed by: for k in F:  D[k] = F[k]
+"""
