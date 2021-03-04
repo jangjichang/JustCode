4a6783b4a0899d5c1864db994cc2fa5d4d37dbdb
JichangJang <wlckd90@gmail.com>
2021-03-05 01:09:32 +0900

    어노테이션 추가

---
+++ python_clean_code/ch01/03_annotations.py
@@ -0,0 +1,12 @@
+class Point:
+    def __init__(self, lat, long):
+        self.lat = lat
+        self.long = long
+
+
+def locate(latitude: float, longitude: float) -> Point:
+    """맵에서 좌표에 해당하는 객체를 검색"""
+    pass
+
+
+print(locate.__annotations__)
