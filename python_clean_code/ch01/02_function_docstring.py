123777ed7ed21dac447a32615f57ffd623a7c265
JichangJang <wlckd90@gmail.com>
2021-03-05 00:16:50 +0900

    black 테스트를 위한 코드 추가

---
+++ python_clean_code/ch01/02_function_docstring.py
@@ -0,0 +1,27 @@
+def my_function():
+    """임의의 계산 수행"""
+    return None
+
+
+print(my_function.__doc__)
+
+
+class Foo(object):
+    def f(self):
+        return 37 * -2
+
+    def g(self, x, y=42):
+        return y
+
+
+def f(a: List[int]):
+    return 37 - a[42 - u : y ** 3]
+
+
+def very_important_function(template: str, *variables, file: os.PathLike, debug: bool = False):
+    """Applies `variables` to the `template` and writes to `file`."""
+    with open(file, "w") as f:
+        ...
+
+
+regular_formatting = [0, 1, 2, 3, 4, 5, 6, 7, 8]
