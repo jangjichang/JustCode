77d1eb1f87d7cf3812dc4a4c283b72bb72964f52
JichangJang <wlckd90@gmail.com>
2021-04-21 09:03:30 +0900

    방어적 프로그래밍

---
+++ python_clean_code/ch03/02_exception_in_abstract_bad_case.py
@@ -0,0 +1,39 @@
+import time
+import logging
+
+
+class DataTransport:
+    """다른 레벨에서 예외를 처리하는 객체의 예"""
+
+    retry_threshold: int = 5
+    retry_n_times: int = 3
+
+    def __init__(self, connector):
+        self._connector = connector
+        self.connection = None
+
+    def deliver_event(self, event):
+        try:
+            self.connect()
+            data = event.decode()
+            self.send(data)
+        except ConnectionError as e:
+            logging.info("연결 실패: %s", e)
+            raise
+        except ValueError as e:
+            logging.info("%r 잘못된 데이터 포함: %s", event, e)
+            raise
+
+    def connect(self):
+        for _ in range(self.retry_n_times):
+            try:
+                self.connection = self._connector.connect()
+            except ConnectionError as e:
+                logging.info("%s: 새로운 연결 시도 %is", e, self.retry_threshold)
+                time.sleep(self.retry_threshold)
+            else:
+                return self.connection
+        raise ConnectionError(f"{self.retry_n_times} 번째 재시도 연결 실패")
+
+    def send(self, data):
+        self.connection.send(data)
