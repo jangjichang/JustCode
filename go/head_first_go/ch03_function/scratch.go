ad61de9ede5192108d9038aa7e9bcfe3cd526577
JichangJang <wlckd90@gmail.com>
2022-01-30 18:41:04 +0900

    study: go pointer

--- go/head_first_go/ch03_function/scratch.go
+++ go/head_first_go/ch03_function/scratch.go
@@ -1,1 +1,13 @@
-package ch03_function
+package main
+
+import "fmt"
+
+func createPointer() *float64 {
+	var myFloat = 98.5
+	return &myFloat
+}
+
+func main() {
+	var myFloatPointer *float64 = createPointer()
+	fmt.Println(*myFloatPointer)
+}
